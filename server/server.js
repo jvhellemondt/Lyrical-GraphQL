8bdf974eaa0f00275fd986f5202d82052451a10c
unknown <jens.vanhellemondt@gmail.com>
2020-06-25 13:23:20 +0200

    Init

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI =
+  'mongodb+srv://Jens:iObKNYzb1yW8cTiB@histlyrical-o4cz5.mongodb.net/test?retryWrites=true&w=majority'
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
@@ -31,3 +32,5 @@ const webpackConfig = require('../webpack.config.js');
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
+
+
